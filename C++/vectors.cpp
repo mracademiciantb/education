42535ea6acd679326c53a45229f8945462b4b8fa
Tommybucksf <firstsamurais@gmail.com>
2023-10-12 20:54:33 +0300

    another one

--- C++/vectors.cpp
+++ C++/vectors.cpp
@@ -6,6 +6,9 @@ int main() {
 
   //nubrer vector
   std::vector<double> subway_adult = {800, 1200, 1500};
+  // What's the last element?   std::cout << name [2];
+  std::cout << subway_adult[0] << "\n";
+  
   std::vector<std::string> employees;
 
 employees.push_back("michael");
@@ -16,7 +19,39 @@ employees.push_back("dwight");
 //amout
 std::cout << "employees.size: " << employees.size();
   
-// What's the last element?   std::cout << name [2];
-  std::cout << "\n" << subway_adult[0];
+
+
+std::vector<int> grades(3);
+  
+  grades[0] = 90;
+  grades[1] = 86;
+  grades[2] = 98;
+
   return 0;
 }
+
+/*#include <iostream>
+#include <vector>
+  
+int main() {
+  
+  std::vector<double> delivery_order;
+  
+  delivery_order.push_back(8.99);
+  delivery_order.push_back(3.75);
+  delivery_order.push_back(0.99);
+  delivery_order.push_back(5.99);
+  
+  // Calculate the total using a for loop:
+  
+  double total = 0.0;
+  
+  for (int i = 0; i < delivery_order.size(); i++) {
+
+    total = total + delivery_order[i];
+  
+  }
+  
+  std::cout << "Total: $" << total << "\n";
+    
+}*/
---
+++ C++/work_with_function.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+
+// Define average() here:
+double average(double num1, double num2) {
+
+  return (num1+num2)/2.0;
+
+}
+
+int main() {
+  
+  std::cout << average(42.0, 24.0) << "\n";
+  std::cout << average(1.0, 2.0) << "\n";
+  
+}
+*/
+#include <iostream>
+#include <cmath>
+
+// Define tenth_power() here:
+int tenth_power(int num) {
+  return pow(num, 10);
+}
+
+int main() {
+  
+  std::cout << tenth_power(0) << "\n";
+  std::cout << tenth_power(1) << "\n";
+  std::cout << tenth_power(2) << "\n";
+  
+}
