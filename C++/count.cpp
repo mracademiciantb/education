c1aa90e8ac27d0a5e2b9093a0d8ab6e45f6b8320
Tommybucksf <firstsamurais@gmail.com>
2023-10-23 17:16:06 +0300

    new lesson yt

---
+++ C++/count.cpp
@@ -0,0 +1,28 @@
+// determines the squares of the numbers the user enters and outputs them, except for the numbers 13 and 666.
+#include <iostream>
+
+int main() {
+    using namespace std; 
+    int N, x;
+
+    cin >> N;
+    /*
+    for (int i=0; i < N; ++i) {
+        cout << "enter number:";
+        cin >> x;
+        if (x == 13 || x == 666)
+            continue;
+        
+        cout << "the square of a number " << x*x << '\n';
+    }
+    */
+    // по закону де Моргана
+    for (int i=0; i < N; ++i) {
+        cout << "enter number:";
+        cin >> x;
+        if (x != 13 && x != 666)
+           
+        cout << "the square of a number " << x*x << '\n';
+    }
+
+    return 0;
